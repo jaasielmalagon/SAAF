53f55176d5a32338b3f86d17a45d4bec052e7201
jaasielmalagon <jaasiel.mendez@alumno.uttehuacan.edu.mx>
2018-05-24 11:06:33 -0500

    Vista cobrar se movió a 'views'´, ahí van todas las pantallas. También se renombró Ejemplo -> Cobrar

--- views/Cobrar.java
+++ views/Cobrar.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Cobro;
+package views;
 
 /**
  *
  * @author 76053
  */
-public class Ejemplo extends javax.swing.JDialog {
+public class Cobrar extends javax.swing.JDialog {
 
     /**
      * Creates new form Ejemplo
      */
-    public Ejemplo(java.awt.Frame parent, boolean modal) {
+    public Cobrar(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -284,20 +284,21 @@ public class Ejemplo extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Ejemplo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cobrar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Ejemplo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cobrar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Ejemplo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cobrar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Ejemplo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cobrar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                Ejemplo dialog = new Ejemplo(new javax.swing.JFrame(), true);
+                Cobrar dialog = new Cobrar(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
