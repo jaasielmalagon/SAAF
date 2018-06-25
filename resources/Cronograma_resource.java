462f14a200af0ea45e9a858ed1c0335c1e47f2a6
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2018-06-24 21:36:11 -0500

    Comentando y modificando clases Cronograma_resource y Cronograma_service

--- resources/Cronograma_resource.java
+++ resources/Cronograma_resource.java
@@ -21,11 +21,16 @@ public class Cronograma_resource {
         this.db = new conection();
     }
     
+    /*Realiza una búsqueda en la base de datos para traer todas las tareas programadas durante el año
+    *@params: Ninguno
+    *@return: tareas: Matriz que contiene las tareas de todo el año
+    *@since: 1.0
+    */
     public String[][] tareasDeTodoElAnio(){
         String[][] tareas = null;
         try{
             this.db.Connect();
-            resultados = this.db.fullSelect("SELECT * FROM tareas");
+            resultados = this.db.fullSelect("SELECT * FROM tareas ORDER BY Fecha");
             if(resultados != null){
                 int i = 0;
                 while(resultados.next()){
@@ -40,6 +45,12 @@ public class Cronograma_resource {
         return tareas;
     }
 
+    /*Realiza una búsqueda en la base de datos para regresar las tareas programadas de una fecha en específico.
+    *El parámetro fecha debe ser ingresado con el siguiente formato yyyy:mm:dd
+    *@param fecha Fecha de las tareas que se desean buscar,
+    *@return tareas: Matriz de cadenas que contiene todas las tareas del parámetro fecha.: 
+    *@since 1.0
+    */
     public String[][] tareasDeUnaFecha(String fecha){
        String[][] tareas = null;
        try{
@@ -61,18 +72,35 @@ public class Cronograma_resource {
        return tareas;
     }
     
+    /*Ingresa una tarea dentro de la base de datos.
+    *El parámetro fecha debe tener el siguiente formato yyyy:mm:dd
+    *@params fecha: Fecha de la tarea que se desea registrar, descripcion: Descripción de la tarea que se registrará.
+    *@return resultado: Variable booleana que indica si la inserción en la base de datos fue exitosa.
+    *@since 1.0
+    */
     public boolean ingresarTarea(String fecha, String descripcion){
         this.db.Connect();
         boolean resultado = this.db.Insert("tareas","`fecha`, `descripcion`", " '"+ fecha +"', '"+ descripcion +"'");
         return resultado;
     }
     
+    /*Modifica una tarea con un id en específico.
+    *El parámetro fecha debe estar en el siguiente formato yyyy:mm:dd
+    *@params fecha: Nueva fecha de la tarea a modificar, descripcion: Nueva descripción de la tarea a modificar.
+    *@return resultado: Variable booleana que indica si la modificación de la tarea fue exitosa.
+    *@since 1.0
+    */
     public boolean modificarTarea(int idTarea, String fecha, String descripcion){
         this.db.Connect();
         boolean resultado =  this.db.Update("tareas", "Fecha = " + fecha + ", Descripcion = " + descripcion,"idTarea = " + idTarea );
         return resultado;
     }
     
+    /*Borra una tarea de la base de datos mediante un Id
+    *@params Id: identificador de la fecha que será eliminada.
+    *@return resultado: Variable booleana que indica si la eliminación de la tarea fue exitosa.
+    *@since 1.0
+    */
     public boolean borrarTarea(int id){
         this.db.Connect();
         boolean resultado = this.db.Delete("tareas", "idTarea = "  + id );
--- services/Cronograma_service.java
+++ services/Cronograma_service.java
@@ -17,12 +17,23 @@ public class Cronograma_service {
         this.recurso = new Cronograma_resource();
     }
     
+    /*Regresa un objeto DefaultTableModel con las tareas de todo el año registradas en la base de datos.
+    *@params Ninguno
+    *@return new DefaultTableModel: Objeto que contiene todas las tareas del año y será usado para llenar un objeto JTable
+    *@since 1.0
+    */
     public DefaultTableModel tablaTareasDeTodoElAnio(){
         String[][] datos = this.recurso.tareasDeTodoElAnio();
         String[] campos = {"Fecha","Descripción"};
         return (datos != null && campos != null) ? new DefaultTableModel(datos, campos) : null;
     }
     
+    /*Regresa un objeto DefaultTableModel con las tareas de una fecha en específico registradas en la base de datos.
+    *El parámetro fecha debe estar en el siguiente formato: yyyy:mm:dd
+    *@params fecha: Fecha de las tareas  que se desean buscar
+    *@return new DefaultTableModel: Objeto que contiene todas las tareas del parámetro fecha y que será usado para llenar un objeto JTable 
+    *@since 1.0
+    */
     public DefaultTableModel tablaTareasDeUnaFecha(String fecha){
         String[][] datos = this.recurso.tareasDeUnaFecha(fecha);
         String[] campos = {"Fecha","Descripción"};
